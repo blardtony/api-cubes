7bd3aef3a752819cc370f11b87530099a79a5ce9
Tony <tonyblard55@gmail.com>
2021-01-19 13:45:51 +0100

    Configuration make, security, dataFixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+/**
+ * Class AppFixtures
+ * @packages App\DataFixtures
+ */
+class AppFixtures extends Fixture
+{
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
